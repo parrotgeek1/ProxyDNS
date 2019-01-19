fa4dc58b38f47fb42d40b8982cb3aaaea283bf5b
rampageX <punkdm@gmail.com>
2019-01-19 16:39:12 +0800

    user can define listen port
    user can define listen port, and change upsteam dns server to OpenDNS.

--- proxydns.c
+++ proxydns.c
@@ -1,6 +1,7 @@
-#define CFG_HOST "8.8.8.8"
-#define CFG_PORT 5353
+#define CFG_HOST "208.67.222.222"
+#define CFG_PORT 443
 #define CFG_IP "ip=dhcp"
+#define CFG_LPORT 6666
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -55,8 +56,8 @@ void handle(int client, char *host, int port)
     unsigned int disconnected = 0;
     fd_set set;
     unsigned int max_sock;
-    
-    
+
+
     /* Create the socket */
     server = socket(PF_INET,SOCK_STREAM,IPPROTO_IP);
     if (server == -1) {
@@ -68,21 +69,21 @@ void handle(int client, char *host, int port)
     serv_addr.sin_addr.s_addr = inet_addr(host);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
-    
+
     /* Connect to the host */
     if (connect(server, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
         perror("TCP error: connect");
         close(client);
         return;
     }
-    
+
     if (client > server) {
         max_sock = client;
     }
     else {
         max_sock = server;
     }
-    
+
     /* Main transfer loop */
     while (!disconnected) {
         FD_ZERO(&set);
@@ -103,19 +104,21 @@ void handle(int client, char *host, int port)
     close(client);
 }
 
-void udpthread(char *ip, int port) {
+void udpthread(char *ip, int port, int lport)
+{
+    lport = CFG_LPORT;
     int os=socket(PF_INET,SOCK_DGRAM,IPPROTO_IP);
-    
+
     struct sockaddr_in a;
     a.sin_family=AF_INET;
-    a.sin_addr.s_addr=inet_addr("0.0.0.0"); a.sin_port=htons(53);
+    a.sin_addr.s_addr=inet_addr("0.0.0.0"); a.sin_port=htons(lport);
     if(bind(os,(struct sockaddr *)&a,sizeof(a)) == -1) {
         perror("UDP error: bind");
         exit(1);
     }
-    
+
     a.sin_addr.s_addr=inet_addr(ip); a.sin_port=htons(port);
-    
+
     struct sockaddr_in sa;
     struct sockaddr_in da; da.sin_addr.s_addr=0;
     puts("Started UDP thread");
@@ -124,7 +127,7 @@ void udpthread(char *ip, int port) {
         socklen_t sn=sizeof(sa);
         int n=recvfrom(os,buf,sizeof(buf),0,(struct sockaddr *)&sa,&sn);
         if(n<=0) continue;
-        
+
         if(sa.sin_addr.s_addr==a.sin_addr.s_addr && sa.sin_port==a.sin_port) {
             if(da.sin_addr.s_addr) sendto(os,buf,n,0,(struct sockaddr *)&da,sizeof(da));
         } else {
@@ -141,8 +144,10 @@ int main(int argc, char **argv)
     int reuseaddr = 1; /* True */
     char * host;
     int port;
+    int lport;
     host = CFG_HOST;
     port = CFG_PORT;
+    lport = CFG_LPORT;
     puts("ProxyDNS "
 #ifdef EMBEDDED
     "OS "
@@ -157,7 +162,7 @@ int main(int argc, char **argv)
     puts("Waiting 2 seconds for network device");
     sleep(2);
     pid_t pidip = fork();
-    
+
     if (pidip == -1) {
         // fork failed
         perror("fork");
@@ -172,16 +177,17 @@ int main(int argc, char **argv)
     }
 #else
     /* Get the server host and port from the command line */
-    if (argc < 3 || argc > 4 || atoi(argv[2]) <= 0) {
-        fprintf(stderr, "Usage: %s host port [-d]\n",argv[0]);
+    if (argc < 4 || argc > 5 || atoi(argv[2]) <= 0) {
+        fprintf(stderr, "Usage: %s host port lport [-d]\n",argv[0]);
         return 1;
     }
     host = argv[1];
     port = atoi(argv[2]);
+    lport = atoi(argv[3]);
 #endif
-    printf("Using proxy DNS server at %s port %d\n",host,port);
+    printf("Using proxy DNS server from %s port %d at %d\n",host,port,lport);
 #ifndef EMBEDDED
-    if(argc == 4 && strcmp(argv[3],"-d") == 0) {
+    if(argc == 5 && strcmp(argv[4],"-d") == 0) {
         puts("Becoming a daemon");
         daemon(0,0);
     }
@@ -195,12 +201,12 @@ int main(int argc, char **argv)
     /* Enable the socket to reuse the address */
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
         perror("TCP error: setsockopt");
-        
+
         return 1;
     }
     struct sockaddr_in atcp;
     atcp.sin_family=AF_INET;
-    atcp.sin_addr.s_addr=inet_addr("0.0.0.0"); atcp.sin_port=htons(53);
+    atcp.sin_addr.s_addr=inet_addr("0.0.0.0"); atcp.sin_port=htons(lport);
     /* Bind to the address */
     if (bind(sock, (struct sockaddr *)&atcp,sizeof(atcp)) == -1) {
         perror("TCP error: bind");
@@ -216,7 +222,7 @@ int main(int argc, char **argv)
     pid_t pid = fork();
     if (pid == 0) {
         // child process
-        udpthread(host,port);
+        udpthread(host,port,lport);
     } else if (pid > 0) {
         /* Main loop */
         puts("Started TCP thread");
