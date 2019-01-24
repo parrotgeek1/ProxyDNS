fdf8d1c01d24e775761f9ea2d8c3ac4e8e82f80f
parrotgeek1 <parrotgeek1@gmail.com>
2019-01-23 23:02:06 -0800

    Delete duplicate scripts, fix permissions

--- make_embedded.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${CCPREFIX}gcc proxydns.c -O3 -Wall -Wextra -Werror -Wno-deprecated-declarations -Wno-unused-parameter -DEMBEDDED -static -s -o proxydns
--- make_static.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-${CCPREFIX}gcc proxydns.c -O3 -Wall -Wextra -Werror -Wno-deprecated-declarations -Wno-unused-parameter -static -s -o proxydns
