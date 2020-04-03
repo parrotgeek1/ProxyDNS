8d53029b9e208a899abd4cd236b0094c73d81334
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2020-04-03 12:17:29 -0500

    fix compilation with newer gcc (remove date/time)

--- proxydns.c
+++ proxydns.c
@@ -152,7 +152,7 @@ int main(int argc, char **argv)
 #ifdef EMBEDDED
     "OS "
 #endif
-"v1.0.3, built on " __DATE__ " at " __TIME__);
+"v1.0.4");
 #ifdef EMBEDDED
     printf("\e[1;1H\e[2J"); // clear spurious vchiq errors
     nice(-20);
